cfafeb7bc9f9e7db8d1a23645b31ba0c24b1b7b2
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2014-06-26 11:05:38 -0500

    Remove polymake cruft from GTrunner

--- GTrunner.py
+++ GTrunner.py
@@ -10,18 +10,17 @@ def main(argv):
     # Set up parameters
     parser = argparse.ArgumentParser(description="Run GTfold with specified a, b, c, d parameters for iB4e")
     parser.add_argument("-s", "--sequence", nargs=1, help="Sequence to fold", required=True)
-    parser.add_argument("-o", "--polyfile", nargs=1, help="Name for polymake output", required=True)
     parser.add_argument("-p", "--paramfile", nargs='?', help="Multibranch vector file (or give parameters on stdin", default=sys.stdin, type=argparse.FileType('r'))
 
     args = vars(parser.parse_args())
     paramfile = args["paramfile"]
-    polyfile = args["polyfile"][0]
     seqfile = args["sequence"][0]
 
     # First, we set up an environment for GTfold
     GTsetMBparam.setup_gt_from_file(turnerdir, outputdir, paramfile)
 
-    run_gt(turnerdir, outputdir, seqfile)
+    structfile = run_gt(turnerdir, outputdir, seqfile)
+    print "Structure stored in " + structfile
 
 def run_gt(turnerdir, outputdir, seqfile):
     # Then we run GTfold on the specified sequence
@@ -29,13 +28,6 @@ def run_gt(turnerdir, outputdir, seqfile):
 
     structfile = os.path.splitext(seqfile)[0] + ".ct"
     return structfile
-
-def write_polyfile_vector(vector, polyfile):
-    # Write out the vector in Polymake format with homogenous coordinates
-    file = open(polyfile, mode='a')
-    vector_string = "[1," + ",".join(map(str, vector)) + "],"
-    file.write(vector_string)
-    file.close()
 
 # Voodoo to make Python run the program
 if __name__ == "__main__":
