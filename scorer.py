52185f6f78ee3a934ba0738cde7c2604f354a9f6
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2014-08-04 15:16:39 -0400

    Add new scoring library and program

---
+++ scorer.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import os, sys, argparse, logging
+from tree import PartitionIntervalNode
+
+def main(argv):
+    # Set up parameters
+    parser = argparse.ArgumentParser(description="Score a specified RNA secondary structure")
+    parser.add_argument("-c", "--structure", nargs=1, help="Structure to score (in CT format)", required=True)
+    parser.add_argument("-v", "--verbose", help="Output debugging information", action="store_true")
+
+    args = vars(parser.parse_args())
+    structfile = args["structure"][0]
+    verbose = args["verbose"]
+
+    logger = logging.getLogger()
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    score_file(structfile)
+
+def score_file(structfile):
+    pairs, seqlength = parse_ct_file(structfile)
+    tree = build_tree(pairs, seqlength)
+    scores = score_tree(tree)
+    logging.info("Tree scores: x = {0}, y = {1}, z = {2}".format(scores[0], scores[1], scores[2]))
+    return scores
+
+# Read in a structure in CT format and produce a list of ordered pairs
+def parse_ct_file(structfile):
+    logging.debug("Opening structure file " + str(structfile))
+    f = open(structfile, 'r')
+    
+    def lineparser(line):
+        # Lines not representing bases are short, so we can catch them using IndexError
+        try:
+            terms = line.split()
+            i, j = int(terms[0]), int(terms[4])
+        except IndexError:
+            return None
+
+        # Drop pairs involving 0 or which are out of order
+        if i < j and j != 0:
+            return (i, j)
+        else:
+            return None
+
+    # The first line contains the number of pairs
+    seqlength = int(f.readline().split()[0])
+    
+    # The rest of the lines encode pairings
+    pairs = [lineparser(line) for line in f if lineparser(line) is not None]
+    f.close()
+
+    logging.debug("Parsed structure file successfully.")
+    
+    return pairs, seqlength
+
+def build_tree(pairs, seqlength):
+    logging.debug("Building tree from sequence of length " + str(seqlength))
+    tree = PartitionIntervalNode(1, seqlength)
+    for pair in pairs:
+        tree.insert(pair[0], pair[1])
+
+    logging.debug("Sorting tree")
+    tree.sort()
+    return tree
+
+def score_tree(tree):
+    logging.debug("Scoring tree")
+    x = 0 # Multiloop counter
+    y = 0 # Unpaired base counter
+    z = 0 # Branched helix counter
+    if tree.valency() >= 2:
+        x = 1
+        y = tree.end - tree.start - 1 - sum(child.end - child.start + 1 for child in tree.children)
+        z = tree.valency() + 1
+
+    child_scores = [score_tree(child) for child in tree.children]
+
+    x += sum(scores[0] for scores in child_scores)
+    y += sum(scores[1] for scores in child_scores)
+    z += sum(scores[2] for scores in child_scores)
+
+    logging.debug("Tree scores: x = {0}, y = {1}, z = {2}".format(x, y, z))
+
+    return x, y, z
+
+# Voodoo to make Python run the program
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ tree.py
@@ -0,0 +1,62 @@
+class PartitionIntervalNode:
+    """
+    Node in a partition interval search tree
+
+    Partition interval search trees require that each interval except the root
+    be a strict subinterval of its parent (the "nestedness" property).
+    If this assumption is not satisfied, we will raise an exception.
+    """
+    def __init__(self, start, end):
+        """
+        Node constructor
+
+        @param start start point of interval
+        @param end end point of interval
+        """
+        self.start = start
+        self.end = end
+        self.children = []
+
+    def __repr__(self):
+        return "Node ({0}, {1})".format(self.start, self.end)
+
+    def print_tree(self, level=0):
+        """
+        Print a schematic representation of the tree descending from `self`
+        """
+        # This is super-ugly right now, but it gets the job done for testing
+        print " "*level + u"\u2520\u2500" + str(self)
+        if len(self.children) != 0:
+            print " "*level + u"\u2517\u2513"
+            for child in self.children:
+                child.print_tree(level+1)
+
+    def insert(self, start, end):
+        """
+        Insert new note as child of this node
+
+        @param start start point of interval
+        @param end end point of interval
+        """
+        # If nestedness fails, raise an exception
+        if start <= self.start or end >= self.end or start >= end:
+            raise ValueError("Bad start or end values")
+
+        assigned = False
+        for child in self.children:
+            # This is where we assume that nestedness holds
+            if child.start < start < child.end:
+                child.insert(start, end)
+                assigned = True
+                break
+
+        if not assigned:
+            self.children.append( PartitionIntervalNode(start, end) )
+
+    def valency(self):
+        return len(self.children)
+
+    def sort(self):
+        self.children.sort()
+        for child in self.children:
+            child.sort()
