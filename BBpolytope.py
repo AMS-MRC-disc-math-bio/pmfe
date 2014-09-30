68a44ba9b2fb4e5ae6f4310268a4fd60812996c3
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2014-09-30 12:18:05 -0400

    Correct bad assumption about ordering in NormalFan()

--- BBpolytope.py
+++ BBpolytope.py
@@ -1,31 +1,5 @@
 from sage.all import *
 
-def affine_hull(points, dim = None):
-    from sage.geometry.hyperplane_arrangement.affine_subspace import AffineSubspace
-
-    if dim == None:
-        dim = len(points[0])
-
-    ambient_space = VectorSpace(QQ, dim)
-    vectors = [ambient_space(point) for point in points]
-
-    shifted_vectors = [vector - vectors[0] for vector in vectors]
-    shifted_linear_hull = ambient_space.subspace(shifted_vectors)
-
-    the_affine_hull = AffineSubspace(vectors[0], shifted_linear_hull)
-    return the_affine_hull
-
-def facet_normal_vector(points, dim):
-    ambient_space = VectorSpace(QQ, dim)
-    vectors = [ambient_space(point) for point in points]
-    center = Polyhedron(vertices = vectors).center()
-
-    shifted_vectors = [vector - vectors[0] for vector in vectors]
-    shifted_linear_hull = ambient_space.subspace(shifted_vectors)
-    normal_space = shifted_linear_hull.complement()
-
-    return normal_space.basis()[0]
-
 def build_polytope(find_vector_oracle, dim):
     from itertools import chain
     ### INITIALIZE
@@ -38,25 +12,25 @@ def build_polytope(find_vector_oracle, dim):
     tentative_polytope = Polyhedron(vertices = initial_vectors)
 
     ### MAIN LOOP
-    confirmed_facets = []
+    confirmed_facets = set()
     while len(confirmed_facets) < tentative_polytope.n_facets():
-        for index in range(tentative_polytope.n_facets()):
-            facet = tentative_polytope.faces(dim-1)[index]
-            vertex_vector_set = set(tuple(coord for coord in vert.vector()) for vert in facet.vertices())
+        for facet in tentative_polytope.faces(dim-1):
+            vertex_vector_set = frozenset(tuple(coord for coord in vert.vector()) for vert in facet.vertices())
             if vertex_vector_set in confirmed_facets:
                 continue
 
-            # TODO: Make sure this is finding the right normals
-            innernormal = NormalFan(tentative_polytope).rays()[index]
-            outernormal = -innernormal
+            facet_center = facet.as_polyhedron().center()
+
+            #Ugly hack, but the NormalFan doesn't come out in a useful order
+            normal = ambient_space(facet.ambient_Hrepresentation()[0][1:])
             
-            v = find_vector_oracle(outernormal)
-            if v in facet.polyhedron():
-                confirmed_facets.append(vertex_vector_set)
-                continue
-            else:
+            v = ambient_space(find_vector_oracle(normal))
+            if normal.dot_product(v) < normal.dot_product(facet_center) and v not in tentative_polytope:
                 vertexlist = list(tentative_polytope.vertices()) + [v]
                 tentative_polytope = Polyhedron(vertices = vertexlist)
                 break
+            else:
+                confirmed_facets.add(vertex_vector_set)
+                continue
 
     return tentative_polytope
