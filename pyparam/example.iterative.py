66bd1aff0066c57639225ac98bd5dd3986b68374
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-03-19 18:02:14 -0400

    Correct ".as_rational()" typo in pyparam example

--- pyparam/example.iterative.py
+++ pyparam/example.iterative.py
@@ -18,7 +18,8 @@ for vector in itertools.product([-1, 0, 1], repeat=4):
     pv = pyparam.ParameterVector.from_py_params(vector)
     # and then call mfe_scores with it
     mfe_scores.append(pyparam.get_mfe_scores(seqfile, outfile, pv).as_fractions())
-    ### The .as_fractions() method gets the result as a list of instances of Python's Fraction, a numeric type. A .as_rational() method is also available which gives instances of Sage's Rational class instead.
+    ### The .as_fractions() method gets the result as a list of instances of Python's Fraction, a numeric type.
+    ### A .as_rationals() method is also available which gives instances of Sage's Rational class instead.
 
 # In this case, printing all 81 results isn't super informative, but what the heck?
 # In general, you'll probably have a more useful idea of what to do with these results (such as checking whether they lie inside a pre-generated polytope)
