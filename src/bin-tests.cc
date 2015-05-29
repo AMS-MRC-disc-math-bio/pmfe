34e0468a269bd000b528911dfd4034ad7681761d
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-05-28 22:01:12 -0400

    Set warning log level in tests

--- src/bin-tests.cc
+++ src/bin-tests.cc
@@ -5,5 +5,24 @@
 // All test cases are defined in other files ("test-*.cc") to keep compilation zippy.
 // There should never be any need to modify this file.
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#define BOOST_LOG_DYN_LINK 1 // Fix an issue with dynamic library loading
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
+int main( int argc, char* const argv[] )
+{
+    // Setup
+    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::warning);
+
+    // Run tests
+    int result = Catch::Session().run( argc, argv );
+
+    // Cleanup
+
+    // Finish
+    return result;
+}
